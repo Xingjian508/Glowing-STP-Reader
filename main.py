e0bbb9d2e92a87786fd353eaf14ef4e7c76600a5
Xingjian Wang <69613830+Xingjian508@users.noreply.github.com>
2023-08-31 00:32:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from stp_reader import main
 
 if __name__ == '__main__':
   # Setting up.
-  PRECISION = 2
-  PATH = 'surface.stp'
+  PRECISION = 3
+  PATHS = ['sample_surface.stp', 'hard.stp', 'hud_shell.stp', 'surface.stp']
   TYPES = 'advanced_face', 'vertex_point'
   
   # Executes the program.
-  main(PRECISION, PATH, TYPES)
+  main(PRECISION, f'stp_files/sample_surface.stp', TYPES, out=False)
+  print('Start of Test')
 
+  # NOTE: TEST with test bundle.
+  for i in range(1, 5):
+    print(f'Test: test{i}.stp')
+    main(PRECISION, f'stp_files/test{i}.stp', TYPES)
+    print()
+
+  # NOTE: TEST with real life projects.
+  for PATH in PATHS:
+    print(f'Test: {PATH}')
+    main(PRECISION, f'stp_files/{PATH}', TYPES)
+    print()
+
+  print('End of Test')
