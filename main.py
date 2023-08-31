c9761136a2cb41ea3c37b9d9d173ab00218e9fe3
Xingjian Wang <69613830+Xingjian508@users.noreply.github.com>
2023-08-31 14:23:42 +0800

    Added Code Style to main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# Code Style:
+# 1. All object attributes are shown in the __init__ methods.
+# 2. All helper methods "_func()" are front-underscored and are static methods.
+# 3. All dunder "__func__()" are defined first, then the helpers, then the rest.
+# 4. All classes and methods, including dunders and helpers, have docstrings.
+# 5. All objects can be created without default parameters.
+# 6. Use 2 spaces for a tab, 2 lines for level 1, and 1 line for level 2.
+
+
 from stp_reader import main
 
 
@@ -22,5 +31,3 @@ if __name__ == '__main__':
     print(f'Test: {PATH}')
     main(PRECISION, f'stp_files/{PATH}', TYPES)
     print()
-
-  print('End of Test')
