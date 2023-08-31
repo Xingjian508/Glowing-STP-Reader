f0b5500c8ce5408bbd03ea3329341dd86198ad82
Xingjian Wang <69613830+Xingjian508@users.noreply.github.com>
2023-08-31 14:30:02 +0800

    Added Code Style, Improved Cycle Algo for regular_obj.py

--- regular_obj.py
+++ regular_obj.py
@@ -1,3 +1,12 @@
+# Code Style:
+# 1. All object attributes are shown in the __init__ methods.
+# 2. All helper methods "_func()" are front-underscored and are static methods.
+# 3. All dunder "__func__()" are defined first, then the helpers, then the rest.
+# 4. All classes and methods, including dunders and helpers, have docstrings.
+# 5. All objects can be created without default parameters.
+# 6. Use 2 spaces for a tab, 2 lines for level 1, and 1 line for level 2.
+
+
 import math
 
 
@@ -98,6 +107,11 @@ class Edge:
     """Returns the string representation."""
     s, t = self.start.coordinates, self.end.coordinates
     return f'Edge({s}, {t})'
+
+  def __eq__(self, other):
+    """Checks if two edges are equal."""
+    return self.start == other.start and \
+           self.end == other.end
 
 
 class Plane:
@@ -301,4 +315,3 @@ class Face:
         if self._tri_contains(v, pivot, verts[i], verts[i+1]):
           return True
     return False
-
